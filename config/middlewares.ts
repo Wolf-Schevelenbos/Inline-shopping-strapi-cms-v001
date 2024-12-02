a2bb416c5c1ae305f1a975e9cac07b12db8b6a39
wolf.schevelenbos <wolf.schevelenbos@student.howest.be>
2024-12-02 09:59:30 +0100

    remove cors enabled

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,6 @@ export default [
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
       origin: ["http://localhost:3000", "http://localhost:5173"],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
