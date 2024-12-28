2187f9646b591a4e6fe3d053b1efd74025e050dc
wolf.schevelenbos <wolf.schevelenbos@student.howest.be>
2024-12-28 17:24:57 +0100

    adjust public url

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("PUBLIC_URL", "https://best-fun-1d0368c0b1.strapiapp.com"),
+  // url: env("PUBLIC_URL", "https://best-fun-1d0368c0b1.strapiapp.com"),
+  url: env("PUBLIC_URL", "https://inviting-novelty-15a3128b95.strapiapp.com"),
   app: {
     keys: env.array("APP_KEYS"),
   },
